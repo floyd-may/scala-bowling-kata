b922c4714c8a56eab71afe4776a395ccd607ddc7
Floyd May <floyd-may@github>
2015-02-15 21:46:16 -0600

    gutter game test

--- src/main/scala/game.scala
+++ src/main/scala/game.scala
@@ -1,6 +1,10 @@
 class Game(pRolls: Seq[Int]) {
   val rolls = pRolls;
 
+  def roll(pins: Int) : Game = {
+    new Game(rolls :+ pins);
+  }
+
   def this() = this(Seq[Int]());
 
   def score = 0;
--- src/test/scala/game.scala
+++ src/test/scala/game.scala
@@ -1,15 +1,27 @@
 import org.specs2.mutable._
 
 class GameSpec extends Specification {
-    "Newly-created Game" should {
-        "have no rolls" in {
-            new Game().rolls mustEqual Seq[Int]();
-        }
-
-        "have score of zero" in {
-            new Game().score mustEqual 0;
-        }
+  "Newly-created Game" should {
+    "have no rolls" in {
+      new Game().rolls mustEqual Seq[Int]();
     }
 
-    
+    "have score of zero" in {
+      new Game().score mustEqual 0;
+    }
+  }
+
+  "Gutter Game" should {
+    val gutterGame = Iterator
+      .continually(0)
+      .take(20)
+      .foldLeft(new Game()){ (game, roll) => game.roll(roll) };
+    "have twenty rolls of zero" in {
+      gutterGame.rolls mustEqual (Iterator.continually(0).take(20) toSeq);
+    }
+
+    "have score of zero" in {
+      gutterGame.score mustEqual 0;
+    }
+  }
 }
